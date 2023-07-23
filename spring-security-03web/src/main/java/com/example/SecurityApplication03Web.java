c34842d2717c07bb988cc5ce4a29fe0767959072
minus <trice12345@aliyun.com>
2023-07-23 18:47:30 +0800

    feat(spring-security-03web): 3.1.8 完整示例-前后端不分离

---
+++ spring-security-03web/src/main/java/com/example/SecurityApplication03Web.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring-security-03web
+ * @author minus
+ * @since 2023-07-23 17:16
+ */
+@SpringBootApplication
+public class SecurityApplication03Web {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication03Web.class, args);
+    }
+}
---
+++ spring-security-03web/src/main/java/com/example/dao/UserDao.java
@@ -0,0 +1,19 @@
+package com.example.dao;
+
+import com.example.entity.Role;
+import com.example.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserDao {
+
+
+    //提供根据用户名返回用户方法
+    User loadUserByUsername(String username);
+
+    //提供根据用户id查询用户角色信息方法
+    List<Role> getRolesByUid(Integer uid);
+
+}
