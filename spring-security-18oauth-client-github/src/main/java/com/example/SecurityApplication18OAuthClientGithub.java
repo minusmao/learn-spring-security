e2444f60042bfc7821bfa58fd897c20c4eab491f
minus <trice12345@aliyun.com>
2023-08-03 01:00:21 +0800

    feat(spring-security-18): 12.5 GitHub 授权登录

---
+++ spring-security-18oauth-client-github/src/main/java/com/example/SecurityApplication18OAuthClientGithub.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication18OAuthClientGithub {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication18OAuthClientGithub.class, args);
+    }
+}
